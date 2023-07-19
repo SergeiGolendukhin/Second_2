0769159cf09efba930934e7c38fd9410f27b623c
Sergei Golendukhin <golendukhin.s.001@gmail.com>
2023-07-19 15:49:01 +0500

    edits made

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
-        int x = 379; // На счету у клиента
-        int r = 2430; // Сумма пополнения
+        int balance = 379; // На счету у клиента
+        int refill = 2430; // Сумма пополнения
 
         int percent;
-        if (r >= 1000) {
-            percent = r/100;
+        if (refill >= 1000) {
+            percent = refill / 100;
         } else {
             percent = 0;
         }
-        System.out.println((x) + " Рубля/ей - изначально на счету");
-        System.out.println((r) + " Рубля/ей - сумма пополнения");
+        System.out.println((balance) + " Рубля/ей - изначально на счету");
+        System.out.println((refill) + " Рубля/ей - сумма пополнения");
         System.out.println((percent) + " Бонуса/ов начисленно");
-        int balance = x + r + percent;
-        System.out.println((balance) + " Рубля/ей - итого на счету");
+        int total = balance + refill + percent;
+        System.out.println((total) + " Рубля/ей - итого на счету");
     }
 }
